83e9bdd050e0237364ac2cd4445a5598b8584e43
Trobo <trobonox@gmail.com>
2021-11-28 20:12:26 +0100

    add placeholder page for install guide

---
+++ pages/installation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+    <h1 class="text-4xl">
+      Work in progress
+    </h1>
+    <h2 class="pt-2 text-4xl">
+      🛠
+    </h2>
+    <nuxt-link to="/">
+      Go back home
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
