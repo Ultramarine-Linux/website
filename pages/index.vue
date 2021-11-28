e1b662f2d1649e9fb4fc96a1ceae675bc72119b0
Trobo <trobonox@gmail.com>
2021-11-28 16:37:43 +0100

    adjust html

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen text-gray-900 dark:text-gray-200 bg-gray-100 dark:bg-gray-900">
     <Navbar />
     <div class="content flex flex-col items-center xl:mx-42 lg:mx-40 md:mx-20 px-8 py-4">
-      <div class="hero flex flex-col items-center mt-4">
+      <section id="hero" class="flex flex-col items-center mt-4">
         <h1 class="self-start md:self-center text-6xl font-semibold tracking-wide text-blue-700 dark:text-blue-400">
           Ultramarine Linux
         </h1>
@@ -10,15 +10,19 @@
           A simplified Linux experience for everyone.
         </h2>
         <div class="buttons flex flex-row justify-between mt-4 self-start md:self-center">
-          <button class="flex flex-row justify-center items-center mr-2 px-4 py-2 rounded-lg bg-blue-600 text-gray-50 hover:bg-blue-700">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-            </svg>
-            <p>Download</p>
-          </button>
-          <button class="ml-2 px-4 py-2 rounded-lg bg-transparent text-gray-900 dark:text-gray-50 border-2 border-blue-500 hover:bg-blue-600 hover:text-gray-50">
-            Installation Guide
-          </button>
+          <a href="https://repos.ultramarine-linux.org/builds/Ultramarine-Flagship-21.11.iso" class="flex flex-row justify-center items-center rounded-lg mr-2 px-4 py-2 bg-blue-600 text-gray-50 hover:bg-blue-700">
+            <button class="flex flex-row justify-center items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+              </svg>
+              <p>Download</p>
+            </button>
+          </a>
+          <a href="" class="flex flex-row justify-center items-center rounded-lg">
+            <button class="ml-2 px-4 py-2 rounded-lg bg-transparent text-gray-900 dark:text-gray-50 border-2 border-blue-500 hover:bg-blue-600 hover:text-gray-50">
+              Installation Guide
+            </button>
+          </a>
         </div>
         <div class="screenshot hidden md:block">
           <img src="../static/ultramarine.png" alt="Desktop Environment Screenshot" class="mt-6 w-4/5 lg:h-96 lg:w-auto rounded-md">
@@ -26,7 +30,7 @@
             Latest Release: Ultramarine Flagship 21.11 (Phing Kan)
           </p>
         </div>
-      </div>
+      </section>
     </div>
   </div>
 </template>
