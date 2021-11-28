dbe78d9e86687aa0792177783e9bff1e72f77686
Trobo <trobonox@gmail.com>
2021-11-28 20:12:07 +0100

    add nuxt img for optimized image load

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,8 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
