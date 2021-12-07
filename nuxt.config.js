61dd036133b287c29329a5d607beea1b8ab91e1d
Trobo <trobonox@gmail.com>
2021-12-07 17:21:16 +0100

    fix nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/navbar.css'
+    '@/assets/css/scrollbar.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
