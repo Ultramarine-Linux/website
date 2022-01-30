994e96082747adc61bfdf325e7d5a1b07e192fa5
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2022-01-30 23:06:08 +0700

    Fix many things + Added GNOME (n/a yet) and Pantheon Editions * Updated Logo * Fix WebHorizon Logo * Changed how certain elements displays    * For example, Sponsors are now inline unless on small devices * Copy Button looks better - Turn off the linting because it caused headache
    Co-authored-by: Cappy Ishihara <cappy@cappuchino.xyz>

--- pages/download.vue
+++ pages/download.vue
@@ -1,16 +1,10 @@
 <template>
   <div
-    class="
-      min-h-screen
-      text-gray-900
-      dark:text-gray-200
-      bg-gray-100
-      dark:bg-gray-900
-    "
+    class="min-h-screen text-gray-900 bg-gray-100 dark:text-gray-200 dark:bg-gray-900"
   >
     <Navbar />
     <!-- Add a red banner here to show that the download links are down -->
-    <!-- <div class="bg-red-500 dark:bg-red-700 text-white font-bold py-4 px-4">
+    <!-- <div class="px-4 py-4 font-bold text-white bg-red-500 dark:bg-red-700">
       <p style="text-align: center">
         Due to some major infrastructure issues, the download links and the
         Ultramarine Linux repositories are currently out of service. We are very
@@ -18,89 +12,108 @@
       </p>
     </div> -->
     <div
-      class="
-        flex
-        p-12
-        flex-col
-        md:items-center
-        items-start
-        min-h-screen
-        bg-gray-100
-        dark:bg-gray-900
-        text-gray-900
-        dark:text-gray-100
-      "
+      class="flex flex-col min-h-screen p-12 text-gray-900 bg-gray-100 place-content-center md:items-center dark:bg-gray-900 dark:text-gray-100"
     >
       <h1 class="text-4xl">Pick your flavor of Ultramarine</h1>
       <h2 class="pt-1 text-gray-400">Latest Release: 21.12 (Phing Kan)</h2>
-      <div class="flex flex-col md:flex-row gap-10 pt-12">
-        <div class="flex flex-col items-center">
-          <nuxt-img
-            src="ultramarine.png"
-            alt="Ultramarine Flagship Screenshot"
-            class="w-96 h-auto pb-4 rounded-sm"
-          />
-          <a
-            href="https://lapis.ultramarine-linux.org/pub/ultramarine/35/Flagship/x86_64/iso/"
-            class="flex flex-row rounded-lg"
-          >
-            <button
-              class="
-                px-4
-                py-2
-                rounded-lg
-                bg-transparent
-                font-semibold
-                text-gray-900
-                dark:text-gray-50
-                border-2 border-blue-500
-                hover:bg-blue-600 hover:text-gray-50
-              "
+      <div class="">
+        <div class="grid grid-flow-row-dense grid-cols-1 gap-10 pt-12 md:grid-cols-2 md:flex-row">
+          <div class="flex flex-col items-center">
+            <nuxt-img
+              src="ultramarine.png"
+              alt="Ultramarine Flagship Screenshot"
+              class="h-auto pb-4 rounded-sm w-96"
+            />
+            <a
+              href="https://lapis.ultramarine-linux.org/pub/ultramarine/35/Flagship/x86_64/iso/"
+              class="flex flex-row rounded-lg"
             >
-              Download Flagship Edition
-            </button>
-          </a>
-          <div class="pt-4">
-            <p class="text-lg text-justify max-w-md">
-              The flagship edition with the latest and greatest software and our
-              best polish. Powered by Solus' Budgie Desktop. (Recommended)
-              <!-- maybe will add a wiki reference to what budgie is here, alongside a read further here, redirecting to wiki-->
-            </p>
+              <button
+                class="px-4 py-2 font-semibold text-gray-900 bg-transparent border-2 border-blue-500 rounded-lg dark:text-gray-50 hover:bg-blue-600 hover:text-gray-50"
+              >
+                Download Flagship Edition
+              </button>
+            </a>
+            <div class="pt-4">
+              <p class="max-w-md text-lg text-justify">
+                The flagship edition with the latest and greatest software and our
+                best polish. Powered by Solus' Budgie Desktop. (Recommended)
+                <!-- maybe will add a wiki reference to what budgie is here, alongside a read further here, redirecting to wiki-->
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="flex flex-col items-center">
-          <nuxt-img
-            src="cutefish.png"
-            alt="Ultramarine Cutefish Screenshot"
-            class="w-96 h-auto pb-4 rounded-sm"
-          />
-          <a
-            href="https://lapis.ultramarine-linux.org/pub/ultramarine/35/Cutefish/x86_64/iso/"
-            class="flex flex-row rounded-lg"
-          >
-            <button
-              class="
-                px-4
-                py-2
-                rounded-lg
-                bg-transparent
-                font-semibold
-                text-gray-900
-                dark:text-gray-50
-                border-2 border-blue-500
-                hover:bg-blue-600 hover:text-gray-50
-              "
+
+          <div class="flex flex-col items-center">
+            <nuxt-img
+              src="cutefish.png"
+              alt="Ultramarine Cutefish Screenshot"
+              class="h-auto pb-4 rounded-sm w-96"
+            />
+            <a
+              href="https://lapis.ultramarine-linux.org/pub/ultramarine/35/Cutefish/x86_64/iso/"
+              class="flex flex-row rounded-lg"
             >
-              Download Cutefish Edition
-            </button>
-          </a>
-          <div class="pt-4">
-            <p class="text-lg text-justify max-w-md">
-              Simplicity, beauty, and practicality, with a familiar look. (In
-              early development, only recommended for more advanced users)
-              <!-- maybe will add a wiki reference to what cutefish is here, alongside a read further here, redirecting to wiki-->
-            </p>
+              <button
+                class="px-4 py-2 font-semibold text-gray-900 bg-transparent border-2 border-blue-500 rounded-lg dark:text-gray-50 hover:bg-blue-600 hover:text-gray-50"
+              >
+                Download Cutefish Edition
+              </button>
+            </a>
+            <div class="pt-4">
+              <p class="max-w-md text-lg text-justify">
+                Simplicity, beauty, and practicality, with a familiar look. (In
+                early development, only recommended for more advanced users)
+                <!-- maybe will add a wiki reference to what cutefish is here, alongside a read further here, redirecting to wiki-->
+              </p>
+            </div>
           </div>
+
+          <div class="flex flex-col items-center">
+            <nuxt-img
+              src="cutefish.png"
+              alt="Ultramarine GNOME Screenshot"
+              class="h-auto pb-4 rounded-sm w-96"
+            />
+            <a
+              class="flex flex-row rounded-lg"
+            >
+              <button
+                disabled
+                class="px-4 py-2 font-semibold text-gray-900 bg-transparent border-2 border-gray-500 rounded-lg dark:text-gray-400"
+              >
+                GNOME Edition Not Available Yet
+              </button>
+            </a>
+            <div class="pt-4">
+              <p class="max-w-md text-lg text-justify">
+                GNOME is a simple, elegant, and intuitive desktop environment with a focus on productivity.
+              </p>
+            </div>
+          </div>
+
+          <div class="flex flex-col items-center">
+            <nuxt-img
+              src="ultramarine.png"
+              alt="Ultramarine Pantheon Screenshot"
+              class="h-auto pb-4 rounded-sm w-96"
+            />
+            <a
+              href="https://lapis.ultramarine-linux.org/pub/ultramarine/35/Pantheon/x86_64/iso/"
+              class="flex flex-row rounded-lg"
+            >
+              <button
+                class="px-4 py-2 font-semibold text-gray-900 bg-transparent border-2 border-blue-500 rounded-lg dark:text-gray-50 hover:bg-blue-600 hover:text-gray-50"
+              >
+                Download Pantheon Edition
+              </button>
+            </a>
+            <div class="pt-4">
+              <p class="max-w-md text-lg text-justify">
+                The default desktop of elementary OS focused on elegance and visual appeal.
+              </p>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
@@ -114,9 +127,9 @@
 <script>
 export default {
   methods: {
-    setColorMode (mode) {
-      this.$colorMode.preference = mode
-    }
-  }
-}
+    setColorMode(mode) {
+      this.$colorMode.preference = mode;
+    },
+  },
+};
 </script>
