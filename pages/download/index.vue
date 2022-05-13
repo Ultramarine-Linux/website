c077c85304d69f22538ae2e579ddd530b7c30088
trobo <57040351+trobonox@users.noreply.github.com>
2022-05-13 17:46:27 +0200

    Fix links on dl page in light mode

--- pages/download/index.vue
+++ pages/download/index.vue
@@ -113,11 +113,11 @@
         </div>
       </div>
 
-      <a href="/download/alternatives" class="pt-1 text-gray-400 mt-6">Looking for more downloads? You can find alternative downloads <span class="text-gray-100">here</span>.</a>
+      <a href="/download/alternatives" class="pt-1 text-gray-400 mt-6">Looking for more downloads? You can find alternative downloads <span class="text-gray-800 dark:text-gray-100 hover:text-blue-600 dark:hover:text-blue-200">here</span>.</a>
 
       <h2 class="text-3xl mt-20">Torrents</h2>
       <h2 class="pt-1 text-gray-400">You can also download Ultramarine Builds using Torrent clients from FOSS Torrents which are available
-          <a class="text-gray-100 hover:text-blue-600 dark:hover:text-blue-200"
+          <a class="text-gray-800 dark:text-gray-100 hover:text-blue-600 dark:hover:text-blue-200"
             href="https://fosstorrents.com/distributions/ultramarine-linux/">
               here
             </a>.
