ca781e6fd245a5ea1ba4c5b06fb0927f0af296ba
trobonox <trobonox@users.noreply.github.com>
2022-05-13 18:13:28 +0000

    deploy: 3bdfb979e609c1568e686e145374e5b1b4f183df

---
+++ _nuxt/static/1652465603/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdownload\u002Falternatives","\u002Fwiki","\u002Fdownload"]})
---
+++ _nuxt/static/1652465603/wiki/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652465603",layout:"default",error:a,state:{},serverRendered:b,routePath:"\u002Fwiki",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
