46cafb785c41432ba3a8070a34b1bc3fa9820777
korewaChino <korewaChino@users.noreply.github.com>
2022-05-20 15:54:04 +0000

    deploy: c682ed77d977d8c38b6a464d131e9c30bd722054

--- _nuxt/static/1653062010/wiki/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653062010",layout:"default",error:a,state:{},serverRendered:b,routePath:"\u002Fwiki",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
