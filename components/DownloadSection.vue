7c4c2264aa13c2bb8c1e34e8a6c9b72e768a4009
Trobo <57040351+trobonox@users.noreply.github.com>
2022-05-28 09:17:05 +0200

    Add reusable component for download sections

---
+++ components/DownloadSection.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex flex-col items-center">
+    <nuxt-img
+      :src="screenshot"
+      :alt="'Ultramarine' + editionName + 'Screenshot'"
+      class="h-auto pb-4 rounded-sm w-96"
+      quality="40"
+    />
+
+    <a
+      :href="downloadLink"
+      class="flex flex-row rounded-lg"
+    >
+      <div class="flex flex-col items-center">
+        <button
+          class="px-4 py-2 font-semibold text-gray-900 bg-transparent border-2 border-blue-500 rounded-lg dark:text-gray-50 hover:bg-blue-600 hover:text-gray-50"
+        >
+          Download {{ editionName }} Edition
+        </button>
+        <a :href="checksumLink" class="mt-1 text-gray-400">View Checksum</a>
+      </div>
+    </a>
+    <div class="pt-2">
+      <p class="max-w-md text-lg text-justify">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+const currentMajorVersion = '36'
+const currentMinorVersion = '1.1'
+const lapisBaseLink = 'https://lapis.ultramarine-linux.org/pub/ultramarine'
+
+
+export default {
+  name: 'DownloadSection',
+  props: ['editionName', 'screenshot', 'description'],
+  computed: {
+    baseLink() {
+      return `${lapisBaseLink}/${currentMajorVersion}/${this.editionName}/x86_64/iso`
+    },
+    downloadLink() {
+      return `${this.baseLink}/Ultramarine-${this.editionName}-Live-x86_64-${currentMajorVersion}-${currentMinorVersion}.iso`
+    },
+
+    checksumLink() {
+      return `${this.baseLink}/CHECKSUM`
+    }
+  }
+}
+</script>
