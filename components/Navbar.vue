8bcf089be40e443d7eff99906ce6e543adf7433c
trobonox <57040351+trobonox@users.noreply.github.com>
2022-06-05 14:15:16 +0200

    Adjust responsiveness on smaller devices

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,7 @@
   <header id="nav" class="flex flex-row justify-between items-center max-w-screen-2xl px-8 py-6 mx-auto text-gray-800 dark:text-gray-200 sticky top-0 z-50 bg-clip-padding bg-gray-100 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-40 backdrop-filter backdrop-blur-xl">
     <nuxt-link to="/" class=" flex flex-row items-center font-bold hover:text-blue-600 dark:hover:text-blue-200">
       <img src="../static/ultramarine-logo.svg" alt="Logo" class="w-11 h-auto mr-2">
-      <h1 class="text-xl font-montserrat-bold">
+      <h1 class="text-xl font-montserrat-bold md:block hidden">
         Ultramarine
       </h1>
     </nuxt-link>
