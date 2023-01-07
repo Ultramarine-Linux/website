a4277c07130eac2dfbf61f0dc6d0eb7edbd74cb1
trobonox <57040351+trobonox@users.noreply.github.com>
2023-01-07 18:57:38 +0100

    feat: add arm page and fix back links

--- src/components/BackMenu.vue
+++ src/components/BackMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mb-4 float-left xs:hidden font-bold text-xl">
+  <div class="mb-4 float-left text-gray-500 text-lg w-full hover:text-gray-300">
       <a :href="targetLink">
           <i class="fa fa-angle-left"></i> &nbsp; Back to {{target}}
       </a>
