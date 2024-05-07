113522c994df71c6feaffc693a4f209702b87e53
Lleyton Gray <lleyton@fyralabs.com>
2024-05-07 03:22:35 -0700

    begin

--- public/migrate.sh
+++ public/migrate.sh
@@ -1,104 +1,92 @@
 #!/usr/bin/env bash
 
-# Migrate from Fedora to Ultramarine Linux script
+set -x
 
-# log all output to file
+# Fedora to Ultramarine Linux migration script
+
+# Log all output to a file
 LOG=ultramarine-migrate.log
-exec > >(tee -a "$LOG") 2>&1
+exec > >(tee -a "${LOG}") 2>&1
 
-# get distro data from /etc/os-release
+# Get distro data from /etc/os-release
 os_id=$(grep -E '^ID=' /etc/os-release | sed -e 's/ID=//g')
-# get distro version data from /etc/os-release
 os_version=$(grep -E '^VERSION_ID=' /etc/os-release | sed -e 's/VERSION_ID=//g')
-# if os_id is fedora and os_version is greater than or equal to 35
-if [ "$os_id" = "fedora" ] && [ "$os_version" -ge 35 ]; then
-    # run fedora migration script
-    MIGRATABLE=true
-# elif it's not f35 or newer
-elif [ "$os_id" = "fedora" ] || [ "$os_version" -lt 35 ]; then
-    # set MIGRATABLE to false
-    echo "This script is only for Fedora 35 or newer."
-    MIGRATABLE=false
-elif [ "$os_id" = "ultramarine" ]; then
-    echo "You are already running Ultramarine Linux. Congratulations!"
-    MIGRATABLE=false
+os_variant=$(grep -E '^VARIANT_ID=' /etc/os-release | sed -e 's/VARIANT_ID=//g')
+
+# If the os_id is fedora and os_version is greater than or equal to 35, then permit upgrade, otherwise error
+if [[ ${os_id} = "fedora" ]] && [[ ${os_version} -ge 35 ]]; then
+  MIGRATABLE=true
+elif [[ ${os_id} = "fedora" ]] || [[ ${os_version} -lt 35 ]]; then
+  echo "This script is only for Fedora 35 or newer."
+  MIGRATABLE=false
+elif [[ ${os_id} = "ultramarine" ]]; then
+  echo "You are already running Ultramarine Linux. Congratulations!"
+  MIGRATABLE=false
 else
-    # set MIGRATABLE to false
-    echo "OS $os_id version $os_version is not supported. Please run this script on a copy of Fedora Linux 35 or newer."
-    MIGRATABLE=false
+  echo "OS ${os_id} version ${os_version} is not supported. Please run this script on a copy of Fedora Linux 35 or newer."
+  MIGRATABLE=false
 fi
 
-# if migratable is still false, exit
-if [ "$MIGRATABLE" = false ]; then
-    exit 1
+# If migratable is false, then exit
+if [[ ${MIGRATABLE} = false ]]; then
+  exit 1
 fi
 
-# check root
-#if [ "$(id -u)" != "0" ]; then
-#    echo "This script must be run as root."
-#    exit 1
-#fi
-
-echo "Welcome to the Fedora to Ultramarine Linux migration script!"
-echo "This script will migrate your Fedora system to Ultramarine Linux."
-echo "Please make sure you have a backup of your system before proceeding, as this script will make major changes to your system."
-echo "This script will also update your system to the latest version of Fedora before proceeding. Please make sure you can properly update your system before proceeding."
+echo "Welcome to the Fedora to Ultramarine Linux migration script!
+This script will migrate your Fedora system to Ultramarine Linux.
+Please make sure you have a backup of your system before proceeding, as this script will make major changes to your system.
+This script will also update your system to the latest version of Fedora before proceeding. Please make sure you can properly update your system before proceeding."
 echo
-# prompt
+
 read -p "Are you sure you want to continue? (y/n) " -r REPLY
 
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo "Exiting..."
-    exit 1
+if [[ ${REPLY} =~ ^[Nn]$ ]]; then
+  echo "Exiting..."
+  exit 1
 fi
 
 echo "====Beginning migration..===="
 echo
-# update system
-echo "Updating system..."
-# Make all commands visible
-set -x
-sudo dnf update -y
-set +x
-echo
-echo "Downloading and installing required packages..."
-# find file in http directory
-echo "Installing RPM Fusion..."
-set -x
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-set +x
-echo "Installing the Ultramarine repository..."
-set -x
-pkglist=(
-    'ultramarine-repos-common'
-    'ultramarine-repos'
-)
-sudo dnf --nogpgcheck --repofrompath ultramarine,https://repos.fyralabs.com/um${os_version}/ install -y ${pkglist[@]}
 
-set +x
+echo "Updating system..."
+sudo dnf update -y
+echo
+
+echo "Downloading and installing required packages..."
+
+echo "Installing RPM Fusion..."
+sudo dnf install -y "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${os_version}.noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${os_version}.noarch.rpm"
+
+echo "Installing the Ultramarine repositories..."
+sudo dnf --nogpgcheck --repofrompath "ultramarine,https://repos.fyralabs.com/um${os_version}/" install -y ultramarine-repos-common ultramarine-repos
+
 echo "Repo installation complete."
 echo
 
 echo "Converting to Ultramarine..."
-# convert to ultramarine
-set -x
-sudo dnf swap -y fedora-release-common ultramarine-release-common --allowerasing
+if [[ ${os_variant} = "workstation" ]]; then
+  sudo dnf swap -y fedora-release-common ultramarine-release-gnome --allowerasing
+elif [[ ${os_variant} = "kde" ]]; then
+  sudo dnf swap -y fedora-release-common ultramarine-release-kde --allowerasing
+elif [[ ${os_variant} = "budgie" ]]; then
+  sudo dnf swap -y fedora-release-common ultramarine-release-flagship --allowerasing
+else
+  sudo dnf swap -y fedora-release-common ultramarine-release-common --allowerasing
+fi
 sudo dnf swap -y fedora-logos ultramarine-logos --allowerasing
-set +x
-echo "Migration complete! Please reboot your system."
-echo "The next Linux kernel update will make your system entry appear as Ultramarine Linux, but now you're already running Ultramarine Linux."
+
+echo "Migration complete! Please reboot your system.
+The next Linux kernel update will make your system entry appear as Ultramarine Linux, but now you're already running Ultramarine Linux."
 
 read -p "Would you like to also generate a new initramfs? (generate an Ultramarine boot entry) (y/n) " -r REPLY
 
-set -x
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Generating new initramfs..."
-    # generate new initramfs
-    sudo dracut -f
-    echo "New initramfs generated."
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+  echo "Generating new initramfs..."
+  sudo dracut -f
+  echo "New initramfs generated."
 fi
 
-set +x
-echo "The migration logs can be found at ${LOG}."
-echo "Have fun!"
+echo "The migration logs can be found at ${LOG}.
+Come chat with us at https://discord.gg/5fdPuxTg5Q or https://matrix.to/#/#hub:fyralabs.com.
+Have fun, thank you for using Ultramarine Linux!"
+
