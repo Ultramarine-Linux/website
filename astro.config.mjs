58ac7976c63c8e3e93a7c0af3b08e6dfa964436b
Lleyton Gray <lleyton@fyralabs.com>
2025-01-24 18:19:09 -0800

    Revert "i hate cloudflare"
    This reverts commit a7adc015b7cccd415032b3ef15b8b3b5ef344e1c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,10 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
   site: "https://ultramarine-linux.org",
   integrations: [mdx(), sitemap(), tailwind(), icon(), react()],
-
   redirects: {
     "/release-announcements/40": {
       status: 302,
@@ -23,6 +19,4 @@ export default defineConfig({
         "https://wiki.ultramarine-linux.org/en/release/upgrade-to-ultramarine-40/",
     },
   },
-
-  adapter: cloudflare(),
 });
