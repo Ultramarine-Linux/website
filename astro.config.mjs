a7adc015b7cccd415032b3ef15b8b3b5ef344e1c
Lleyton Gray <lleyton@fyralabs.com>
2025-01-24 18:17:14 -0800

    i hate cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,14 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   site: "https://ultramarine-linux.org",
   integrations: [mdx(), sitemap(), tailwind(), icon(), react()],
+
   redirects: {
     "/release-announcements/40": {
       status: 302,
@@ -19,4 +23,6 @@ export default defineConfig({
         "https://wiki.ultramarine-linux.org/en/release/upgrade-to-ultramarine-40/",
     },
   },
+
+  adapter: cloudflare(),
 });
