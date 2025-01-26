1b60a26e415bd74d3d556353cb9284b01d7435dc
lleyton@fyralabs.com <lleyton@fyralabs.com>
2025-01-26 15:40:38 -0800

    update some links

--- public/migrate.sh
+++ public/migrate.sh
@@ -103,7 +103,6 @@ fi
 
 echo
 echo "The migration logs can be found at ${LOG}.
-Come chat with us at https://discord.gg/5fdPuxTg5Q or https://matrix.to/#/#hub:fyralabs.com.
+Come chat with us at https://fyralabs.com/discord or https://fyralabs.com/matrix.
 For addtional information, please see the Ultramarine Wiki, https://wiki.ultramarine-linux.org.
 Have fun, thank you for using Ultramarine Linux!"
-
