e949b737b77cf59bf4f42b2e5058d4b153004405
lleyton@fyralabs.com <lleyton@fyralabs.com>
2025-04-03 14:36:09 -0700

    no dropdown

--- src/components/DownloadOptions.tsx
+++ src/components/DownloadOptions.tsx
@@ -32,67 +32,60 @@ const DownloadOptions = () => {
   const $downloadDevice = useStore(downloadDevice);
 
   return (
-    <>
-      <details>
-        <summary className="pt-1 text-gray-500 text-sm cursor-pointer">
-          Download Options
-        </summary>
-        <div className="flex flex-row flex-wrap gap-4 pt-2">
-          <div>
-            <p className="pb-1 text-sm text-gray-300">Architecture</p>
-            <ToggleGroup.Root
-              className="inline-flex h-9 items-center justify-center rounded-lg bg-gray-800 p-1 text-muted-foreground"
-              type="single"
-              value={$downloadArch}
-              onValueChange={(value) => {
-                if (value) {
-                  downloadArch.set(value);
-                  downloadDevice.set("generic");
-                }
-              }}
-              aria-label="Architecture"
-            >
-              {Array.from(archDownloadVariations.entries()).map(
-                ([id, archVariation]) => (
-                  <ToggleGroup.Item
-                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md min-w-20 px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-gray-700 data-[state=active]:text-foreground data-[state=active]:shadow"
-                    value={id}
-                    aria-label={archVariation.name}
-                  >
-                    {archVariation.name}
-                  </ToggleGroup.Item>
-                ),
-              )}
-            </ToggleGroup.Root>
-          </div>
+    <div className="flex flex-row flex-wrap gap-4 pt-2">
+      <div>
+        <p className="pb-1 text-sm text-gray-300">Architecture</p>
+        <ToggleGroup.Root
+          className="inline-flex h-9 items-center justify-center rounded-lg bg-gray-800 p-1 text-muted-foreground"
+          type="single"
+          value={$downloadArch}
+          onValueChange={(value) => {
+            if (value) {
+              downloadArch.set(value);
+              downloadDevice.set("generic");
+            }
+          }}
+          aria-label="Architecture"
+        >
+          {Array.from(archDownloadVariations.entries()).map(
+            ([id, archVariation]) => (
+              <ToggleGroup.Item
+                className="inline-flex items-center justify-center whitespace-nowrap rounded-md min-w-20 px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-gray-700 data-[state=active]:text-foreground data-[state=active]:shadow"
+                value={id}
+                aria-label={archVariation.name}
+              >
+                {archVariation.name}
+              </ToggleGroup.Item>
+            ),
+          )}
+        </ToggleGroup.Root>
+      </div>
 
-          <div>
-            <p className="pb-1 text-sm text-gray-300">Device</p>
-            <ToggleGroup.Root
-              className="inline-flex h-9 items-center justify-center rounded-lg bg-gray-800 p-1 text-muted-foreground"
-              type="single"
-              value={$downloadDevice}
-              onValueChange={(value) => {
-                if (value) downloadDevice.set(value);
-              }}
-              aria-label="Devices"
+      <div>
+        <p className="pb-1 text-sm text-gray-300">Device</p>
+        <ToggleGroup.Root
+          className="inline-flex h-9 items-center justify-center rounded-lg bg-gray-800 p-1 text-muted-foreground"
+          type="single"
+          value={$downloadDevice}
+          onValueChange={(value) => {
+            if (value) downloadDevice.set(value);
+          }}
+          aria-label="Devices"
+        >
+          {Array.from(
+            archDownloadVariations.get($downloadArch).devices.entries(),
+          ).map(([id, name]) => (
+            <ToggleGroup.Item
+              className="inline-flex items-center justify-center whitespace-nowrap rounded-md min-w-20 px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-gray-700 data-[state=active]:text-foreground data-[state=active]:shadow"
+              value={id}
+              aria-label={name}
             >
-              {Array.from(
-                archDownloadVariations.get($downloadArch).devices.entries(),
-              ).map(([id, name]) => (
-                <ToggleGroup.Item
-                  className="inline-flex items-center justify-center whitespace-nowrap rounded-md min-w-20 px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-gray-700 data-[state=active]:text-foreground data-[state=active]:shadow"
-                  value={id}
-                  aria-label={name}
-                >
-                  {name}
-                </ToggleGroup.Item>
-              ))}
-            </ToggleGroup.Root>
-          </div>
-        </div>
-      </details>
-    </>
+              {name}
+            </ToggleGroup.Item>
+          ))}
+        </ToggleGroup.Root>
+      </div>
+    </div>
   );
 };
 
