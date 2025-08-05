6310b5cecdc28f5dff0c14fd0be4b4097f78aa5d
Lleyton Gray <lleyton@fyralabs.com>
2025-08-05 00:01:31 -0500

    chore: upgrade to tailwind v4
    Co-authored-by: Jaiden Riordan <jade@fyralabs.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,26 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ultramarine-linux.org",
-  integrations: [mdx(), sitemap(), tailwind(), icon(), react()],
+  integrations: [mdx(), sitemap(), icon(), react()],
+
   redirects: {
     "/release-announcements/40": {
       status: 302,
       destination:
         "https://wiki.ultramarine-linux.org/en/release/upgrade-to-ultramarine-40/",
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
