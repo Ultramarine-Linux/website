f59d838d0fa048967517db9eb4e8579fef913ad5
Lleyton Gray <lleyton@fyralabs.com>
2025-10-04 21:24:07 -0700

    react keying

--- src/components/DownloadOptions.tsx
+++ src/components/DownloadOptions.tsx
@@ -173,6 +173,7 @@ const DownloadOptions = () => {
               ([id, archVariation]) => (
                 <ToggleGroup.Item
                   className="inline-flex items-center justify-center whitespace-nowrap rounded-md min-w-20 px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-gray-700 data-[state=active]:text-foreground data-[state=active]:shadow"
+                  key={id}
                   value={id}
                   aria-label={archVariation.name}
                 >
