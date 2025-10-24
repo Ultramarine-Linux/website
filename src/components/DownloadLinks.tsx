78f0601b8ad0e464bb77253033713fdec474e460
Lleyton Gray <lleyton@fyralabs.com>
2025-10-23 17:08:22 -0700

    download fixes

--- src/components/DownloadLinks.tsx
+++ src/components/DownloadLinks.tsx
@@ -2,13 +2,24 @@ import { useStore } from "@nanostores/react";
 import { downloadArch, downloadDevice } from "./downloadStore";
 import DownloadIcon from "./icons/DownloadIcon.svg?react";
 
-const downloadLink = (version: number, arch: string, device: string) =>
-  `https://images.fyralabs.com/isos/ultramarine/${version}/ultramarine-flagship-${version}-live-${arch}.iso`;
+const downloadLink = (
+  edition: string,
+  version: number,
+  arch: string,
+  device: string,
+) =>
+  `https://images.fyralabs.com/isos/ultramarine/${version}/ultramarine-${edition}-${version}-live-${arch}.iso`;
 
-const DownloadLinks = ({ version }: { version: number }) => {
+const DownloadLinks = ({
+  version,
+  edition,
+}: {
+  version: number;
+  edition: string;
+}) => {
   const arch = useStore(downloadArch);
   const device = useStore(downloadDevice);
-  const link = downloadLink(version, arch, device);
+  const link = downloadLink(edition, version, arch, device);
 
   return (
     <div className="flex flex-col sm:flex-row gap-2 md:gap-4 sm:items-center z-10 mt-6 md:mt-0">
@@ -20,7 +31,7 @@ const DownloadLinks = ({ version }: { version: number }) => {
         <span>Download</span>
       </a>
       <a
-        href={link + ".sha256"}
+        href={link + ".sha256sum"}
         className="text-gray-400 hover:text-blue-400 text-center md:text-left text-xs md:text-base"
       >
         View Checksum
