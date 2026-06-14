e29e96daff4924ffc7ec4a8217ac03415a16c809
Owen Zimmerman <123591347+Owen-sz@users.noreply.github.com>
2026-06-14 08:27:42 -0500

    simplify asahi logic
    Co-authored-by: june <159748074+june-fish@users.noreply.github.com>

--- public/migrate.sh
+++ public/migrate.sh
@@ -24,14 +24,9 @@ os_version=$(grep -E '^VERSION_ID=' /etc/os-release | sed -e 's/VERSION_ID=//g';
 os_variant=$(grep -E '^VARIANT_ID=' /etc/os-release | sed -e 's/VARIANT_ID=//g'; true)
 
 # If the os_id is fedora and os_version is greater than or equal to MINIMUM_RELEASEVER, then permit upgrade, otherwise error
-if [[ ${os_id} = "fedora" ]] && [[ ${os_version} -ge $MINIMUM_RELEASEVER ]]; then
+if [[ ${os_id} = "fedora" || ${os_id} = "fedora-asahi-remix" ]] && [[ ${os_version} -ge $MINIMUM_RELEASEVER ]]; then
   : # do nothing
-elif [[ ${os_id} = "fedora-asahi-remix" ]] && [[ ${os_version} -ge $MINIMUM_RELEASEVER ]]; then
-  : # do nothing
-elif [[ ${os_id} = "fedora" ]] || [[ ${os_version} -lt $MINIMUM_RELEASEVER ]]; then
-  echo "This script is only for Fedora $MINIMUM_RELEASEVER or newer."
-  exit 1
-elif [[ ${os_id} = "fedora-asahi-remix" ]] && [[ ${os_version} -ge $MINIMUM_RELEASEVER ]]; then
+elif [[ ${os_id} = "fedora" || ${os_id} = "fedora-asahi-remix" ]] || [[ ${os_version} -lt $MINIMUM_RELEASEVER ]]; then
   echo "This script is only for Fedora $MINIMUM_RELEASEVER or newer."
   exit 1
 elif [[ ${os_id} = "ultramarine" ]]; then
